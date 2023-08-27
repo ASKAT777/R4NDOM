5141f0059cc1241d0145f3c2b2ff7891e4ef97c9
ASKAT777 <141628250+ASKAT777@users.noreply.github.com>
2023-08-27 08:34:16 +0600

    ASKU_RANDOM2.py

--- ASKU_RANDOM2.py
+++ ASKU_RANDOM2.py
@@ -8,36 +8,28 @@ logo = ("""
 \33[32;1m           ┃\33[35;1m FACEBOOK   => ASKAT MRIDHA          ┃
 \33[32;1m           ┃\33[31;1m      SORRY!  TOOL'S IS OFFED BY ASKAT       ┃
 \33[32;1m           ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+	
+	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
+	
 
-print(logo)
-
-	os.system("clear")
-	print(logo)
-	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
 	
-os.system("clear")
-	print(logo)
-	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
-	
-	os.system("clear")
-	print(logo)
-	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
-	
-	os.system("clear")
-	print(logo)
-	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
-	
-	os.system("clear")
-	print(logo)
-	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
-	
-	os.system("clear")
-	print(logo)
 	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
 	
 	
-	os.system("clear")
-	print(logo)
+	
+	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
+	
+	
+	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
+	
+	
+	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
+	
+	
+	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
+	
+	
+	
 	print("SORRY TOOLS OFF KORTE BADDHO HOLAM ASOLE BAD REVIEW PAISI😒")
 	
 	os.system("clear")
